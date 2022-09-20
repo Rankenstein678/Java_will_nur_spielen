c3878e7777acf1fa94e35e8291423facd0838a11
Phillip Zazzetta <phillip.zazzetta@schiller-og.de>
2022-09-20 09:10:47 +0200

    Test Main file updated

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@ public class Test {
         Vertex v2 = new Vertex(42.0, 37.0);
         Vertex v3 = new Vertex(0, 0);
         System.out.println(v1 + " " + v2 + " " + v3);
+        System.out.println(v1.length());
     }
 }

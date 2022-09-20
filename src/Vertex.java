9bff1b7468d6f24914943c01b48ed809c24c8c65
Phillip Zazzetta <phillip.zazzetta@schiller-og.de>
2022-09-20 09:05:58 +0200

    Vertex updated Test Main file

--- src/Vertex.java
+++ src/Vertex.java
@@ -7,4 +7,31 @@ public class Vertex {
         this.x = x;
         this.y = y;
     }
+
+    public double length() {
+        return Math.sqrt(x * x + y * y);
+    }
+
+    public Vertex add(Vertex v2) {
+        return new Vertex(x + v2.x, y + v2.y);
+    }
+
+    public void addMod(Vertex v2) {
+        x += v2.x;
+        y += v2.y;
+    }
+
+    public Vertex skalarMult(double s) {
+        return new Vertex(x * s, y * s);
+    }
+
+    public void skalarMultMod(double s) {
+        x *= s;
+        y *= s;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }

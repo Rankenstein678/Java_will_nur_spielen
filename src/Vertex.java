51037b37db847a09ea1224182fb8b68a811263b6
PhillipZazzetta <zphillip98@gmail.com>
2022-09-22 15:10:20 +0200

    reformat

--- src/Vertex.java
+++ src/Vertex.java
@@ -12,15 +12,6 @@ public class Vertex {
         return Math.sqrt(x * x + y * y);
     }
 
-    public Vertex add(Vertex v2) {
-        return new Vertex(x + v2.x, y + v2.y);
-    }
-
-    public void addMod(Vertex v2) {
-        x += v2.x;
-        y += v2.y;
-    }
-
     public Vertex skalarMult(double s) {
         return new Vertex(x * s, y * s);
     }
@@ -28,6 +19,15 @@ public class Vertex {
     public void skalarMultMod(double s) {
         x *= s;
         y *= s;
+    }
+
+    public Vertex add(Vertex v2) {
+        return new Vertex(x + v2.x, y + v2.y);
+    }
+
+    public void addMod(Vertex v2) {
+        x += v2.x;
+        y += v2.y;
     }
 
     @Override

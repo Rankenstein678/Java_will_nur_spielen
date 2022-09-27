ff359aa90fbae62f09178458e4fd0a0736b79d17
Phillip Zazzetta <phillip.zazzetta@schiller-og.de>
2022-09-27 08:48:18 +0200

    getter setter equals

--- src/Vertex.java
+++ src/Vertex.java
@@ -25,12 +25,37 @@ public class Vertex {
     }
 
     public void addMod(Vertex v2) {
-        x += v2.x;
-        y += v2.y;
+        x += v2.getX();
+        y += v2.getY();
     }
 
     @Override
     public String toString() {
         return "(" + x + ", " + y + ")";
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object thatObj) {
+        if (thatObj instanceof Vertex) {
+            Vertex that = (Vertex) thatObj;
+            return x == that.x && y == that.y;
+        }
+        return false;
     }
 }

6195355b112f99033839954e7e92e2469b0c6974
Phillip Zazzetta <phillip.zazzetta@schiller-og.de>
2022-09-27 08:53:21 +0200

    equals test

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,11 @@
 public class Test {
     public static void main(String[] args) {
-        Vertex v1 = new Vertex(1.0, 2.0);
-        Vertex v2 = new Vertex(42.0, 37.0);
-        Vertex v3 = new Vertex(0, 0);
+        Vertex v1 = new Vertex(1, 2);
+        Vertex v2 = new Vertex(42, 37);
+        Vertex v3 = new Vertex(1, 2);
+
+        System.out.println(v1.equals(v2));
+        System.out.println(v1.equals(v3));
 
         System.out.println(v1 + " " + v2 + " " + v3);
         System.out.println("length() " + v1.length());
@@ -12,5 +15,6 @@ public class Test {
         System.out.println("add() " + v1.add(v2));
         v1.addMod(v2);
         System.out.println("addMod() " + v1);
+
     }
 }

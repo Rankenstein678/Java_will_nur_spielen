487093849e40022653cd278ecf6afa61a4ae8fd0
Phillip Zazzetta <phillip.zazzetta@schiller-og.de>
2022-09-28 09:10:30 +0200

    GeometricObject toString Test.java rewrite

--- src/Test.java
+++ src/Test.java
@@ -2,12 +2,14 @@ public class Test {
     public static void main(String[] args) {
         GeometricObject g1 = new GeometricObject(new Vertex(1,2),3,4);
         GeometricObject g2 = new GeometricObject(1,2,3,4);
-        GeometricObject g3 = new GeometricObject(1);
-        GeometricObject g4 = new GeometricObject();
+        GeometricObject g3 = new GeometricObject(1,2);
+        GeometricObject g4 = new GeometricObject(1);
+        GeometricObject g5 = new GeometricObject();
 
         System.out.println(g1);
         System.out.println(g2);
         System.out.println(g3);
         System.out.println(g4);
+        System.out.println(g5);
     }
 }

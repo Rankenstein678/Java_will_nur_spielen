e5e67b08590fbebffa3e501d13e956d145f21620
Rankenstein678 <94004557+Rankenstein678@users.noreply.github.com>
2022-10-13 18:45:03 +0200

    PaintableStar.java complete

--- src/PaintableStar.java
+++ src/PaintableStar.java
@@ -1,36 +1,38 @@
 import java.awt.*;
 
-//EXPERIMENTELL!!! NICHT ABGEBEN!!!
-public class PaintableStar implements Paintable {
-    double x, y;
+public class PaintableStar extends GeometricObject implements Paintable {
     public int radInner, radOuter, zacken;
-    public Color color;
 
-    public PaintableStar(double x, double y, int radInner, int radOuter, int zacken, Color color) {
-        this.x = x;
-        this.y = y;
+    public PaintableStar(Vertex pos, int radInner, int radOuter, int zacken, Color color) {
+        super(pos, -1, -1, color);
         this.radInner = radInner;
         this.radOuter = radOuter;
         this.zacken = zacken;
-        this.color = color;
     }
 
-    public PaintableStar(Vertex pos, int radInner, int radOuter, int zacken, Color color) {
-        this(pos.x, pos.y, radInner, radOuter, zacken, color);
+    public PaintableStar(double x, double y, int radInner, int radOuter, int zacken, Color color) {
+        this(new Vertex(x, y), radInner, radOuter, zacken, color);
     }
 
     @Override
     public void paintTo(Graphics g) {
-        double[] xpoly = {x,x+radInner,x+radOuter,x+radInner,x,x-radInner,x-radOuter,x-radInner};
-        double[] ypoly = {y-radOuter,y-radInner,y,y+radInner,y+radOuter,y+radInner,y,y-radInner};
-        final int[] xp = new int[xpoly.length];
-        final int[] yp = new int[ypoly.length];
-        for (int i = 0; i < xpoly.length;i++) xp[i]= (int) xpoly[i];
-        for (int i = 0; i < ypoly.length;i++) yp[i]= (int) ypoly[i];
-        System.out.println(xpoly.length+" "+ypoly.length);
-        Polygon p = new Polygon(xp, yp, xp.length);
+        int[] xpoly = new int[zacken * 2];
+        int[] ypoly = new int[zacken * 2];
+        double degrees = 360.0 / (zacken * 2);
+
+        for (int i = 0; i < zacken * 2; i++) {
+            double deg = Math.toRadians(degrees * i); //Das hat mich viel zu viel Zeit gekostet
+            if (i % 2 == 0) {
+                xpoly[i] = (int) Math.round(pos.x + Math.cos(deg) * radOuter);
+                ypoly[i] = (int) Math.round(pos.y - Math.sin(deg) * radOuter);
+            } else {
+                xpoly[i] = (int) Math.round(pos.x + Math.cos(deg) * radInner);
+                ypoly[i] = (int) Math.round(pos.y - Math.sin(deg) * radInner);
+            }
+
+        }
         g.setColor(color);
+        Polygon p = new Polygon(xpoly, ypoly, xpoly.length);
         g.fillPolygon(p);
-        System.out.println(p.getBounds());
     }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,15 @@
 import util.ShowInFrame;
 
+import java.awt.*;
+
 public class Test {
     public static void main(String[] args) {
 
-        //EXPERIMENTELL!!! NICHT ABGEBEN!!!
-        //ShowInFrame.show("AAAAAAAAAAA", new PaintablePanel(new PaintableStar(100,100,12,50,10, Color.BLUE)));
+        ShowInFrame.show("AAAAAAAAAAA", new PaintablePanel(new PaintableStar(100,100,25,100,12, Color.BLUE)));
 
-        GeometricObject img = new GeometricImage("duck.png",0,0);
-        System.out.println(img.width);
-        System.out.println(img.height);
-        ShowInFrame.show(new PaintablePanel((Paintable) img));
+//        GeometricObject img = new GeometricImage("duck.png",0,0);
+//        System.out.println(img.width);
+//        System.out.println(img.height);
+//        ShowInFrame.show(new PaintablePanel((Paintable) img));
     }
 }

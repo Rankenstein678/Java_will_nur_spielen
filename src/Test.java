a07c5515e4c26415b727fef031cbd84d6e9dcbcb
Rankenstein678 <94004557+Rankenstein678@users.noreply.github.com>
2022-10-13 18:45:45 +0200

    PaintableStar.java complete

--- src/Test.java
+++ src/Test.java
@@ -5,7 +5,7 @@ import java.awt.*;
 public class Test {
     public static void main(String[] args) {
 
-        ShowInFrame.show("AAAAAAAAAAA", new PaintablePanel(new PaintableStar(100,100,25,100,12, Color.BLUE)));
+        ShowInFrame.show(new PaintablePanel(new PaintableStar(100,100,25,100,12, Color.BLUE)));
 
 //        GeometricObject img = new GeometricImage("duck.png",0,0);
 //        System.out.println(img.width);
